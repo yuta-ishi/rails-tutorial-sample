572774f0ce1ee54d33e4c872a5e3d502ce72da5e
yuta-ishi <yu.ishi.25@gmail.com>
2017-03-07 07:39:37 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +17,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    log_in_as(@user)
+    get users_path
+    assert_select "a[href=?]", users_path
+    get users_path @user
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_select 'form[action="/signup"]'
+    assert_select 'form[action="/users"]'
     assert_no_difference 'User.count' do
       post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
@@ -11,7 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'form[action="/signup"]'
+    assert_select 'form[action="/users"]'
     assert_select 'div#error_explanation'
     assert_select 'div#error_explanation ul li', 4
     assert_select 'div.field_with_errors', 8
