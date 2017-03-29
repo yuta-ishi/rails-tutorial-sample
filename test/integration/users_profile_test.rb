baf5a5b13c052f8bcdbaef682298b7c26b094a73
yuta-ishi <yu.ishi.25@gmail.com>
2017-03-29 02:18:40 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_select '#following', /#{@user.following.count}/
+    assert_select '#followers', /#{@user.followers.count}/
   end
 end
